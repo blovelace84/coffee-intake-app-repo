f8c58d89f25ff9435a9d6d93b667cfb7415ddc13
Brittani Lovelace <blovelace84@gmail.com>
2025-10-09 07:58:10 -0400

    update

---
+++ coffee-intake-app/app/src/navigation/AppNavigator.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import AddIntake from "../pages/AddIntake";
+import Stats from "../pages/Stats";
+import { RootStackParamList } from "../types/navigation";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#4b2e05" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} options={{ title: "Welcome" }} />
+        <Stack.Screen name="Home" component={Home} options={{ title: "My Coffee Log" }} />
+        <Stack.Screen name="AddIntake" component={AddIntake} options={{ title: "Add Coffee" }} />
+        <Stack.Screen name="Stats" component={Stats} options={{ title: "My Stats" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
