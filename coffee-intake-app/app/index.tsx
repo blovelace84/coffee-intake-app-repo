d6b6533d46796befe9d9af7fbee8875a363e9983
Brittani Lovelace <blovelace84@gmail.com>
2025-10-15 19:31:42 -0400

    update

--- coffee-intake-app/app/index.tsx
+++ coffee-intake-app/app/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar style="light" />
       <AppNavigator />
-    </>
+    </NavigationContainer>
   );
 }
--- coffee-intake-app/app/src/navigation/AppNavigator.jsx
+++ coffee-intake-app/app/src/navigation/AppNavigator.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import Login from "../screens/Login";
 import Home from "../screens/Home";
 import AddIntake from "../screens/AddIntake";
@@ -12,36 +10,34 @@ const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{
-          headerStyle: { backgroundColor: "#4b2e05" },
-          headerTintColor: "#fff",
-          headerTitleStyle: { fontWeight: "bold" },
-        }}
-      >
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ title: "Welcome" }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: "My Coffee Log" }}
-        />
-        <Stack.Screen
-          name="AddIntake"
-          component={AddIntake}
-          options={{ title: "Add Coffee" }}
-        />
-        <Stack.Screen
-          name="Stats"
-          component={Stats}
-          options={{ title: "My Stats" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerStyle: { backgroundColor: "#4b2e05" },
+        headerTintColor: "#fff",
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    >
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ title: "Welcome" }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "My Coffee Log" }}
+      />
+      <Stack.Screen
+        name="AddIntake"
+        component={AddIntake}
+        options={{ title: "Add Coffee" }}
+      />
+      <Stack.Screen
+        name="Stats"
+        component={Stats}
+        options={{ title: "My Stats" }}
+      />
+    </Stack.Navigator>
   );
 }
